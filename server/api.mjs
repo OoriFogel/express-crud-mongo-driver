22ab4935c1fb8d9c54f401b621c9ebb75e2fd7bf
OoriFogel <ufogelu@gmail.com>
2023-11-23 19:15:15 +0200

    get all fixed

--- server/api.mjs
+++ server/api.mjs
@@ -1,26 +1,25 @@
 // require('dotenv').config();
-import express from 'express'
-import morgan from 'morgan'
-import log from '@ajar/marker'
-import cors from 'cors'
+import express from 'express';
+import morgan from 'morgan';
+import log from '@ajar/marker';
+import cors from 'cors';
 
-import {connect_db} from './db/mongo.connection.mjs';
+import { connect_db } from './db/mongo.connection.mjs';
 import user_router from './modules/user/user.router.mjs';
 
-import {error_handler,error_handler2,not_found} from './middleware/errors.handler.mjs';   
+import {
+  error_handler,
+  error_handler2,
+  not_found,
+} from './middleware/errors.handler.mjs';
 
-const { 
-  PORT = 8080,
-  HOST = 'localhost', 
-  DB_URI,
-  DB_NAME
- } = process.env;
+const { PORT = 8080, HOST = 'localhost', DB_URI, DB_NAME } = process.env;
 
 const app = express();
 
 // middleware
 app.use(cors());
-app.use(morgan('dev'))
+app.use(morgan('dev'));
 
 // routing
 // app.use('/api/stories', story_router);
@@ -31,12 +30,12 @@ app.use(error_handler);
 app.use(error_handler2);
 
 //when no routes were matched...
-app.use('*', not_found)
+app.use('*', not_found);
 
 //start the express api server
-;(async ()=> {
+(async () => {
   //connect to mongo db
-  await connect_db(DB_URI,DB_NAME);  
-  await app.listen(PORT,HOST);
-  log.magenta(`api is live on`,` ✨ ⚡  http://${HOST}:${PORT} ✨ ⚡`);  
+  await connect_db(DB_URI, DB_NAME);
+  await app.listen(PORT, HOST);
+  log.magenta(`api is live on`, ` ✨ ⚡  http://${HOST}:${PORT} ✨ ⚡`);
 })().catch(log.error);
--- server/modules/user/user.router.mjs
+++ server/modules/user/user.router.mjs
@@ -7,18 +7,18 @@
 // returning the appropriate HTTP status code
 import express from 'express';
 import log from '@ajar/marker';
-import * as user_controller from "./user.controller.mjs";
+import * as user_controller from './user.controller.mjs';
 /* 
   if there is an error thrown in the DB, asyncMiddleware
   will pass it to next() and express will handle the error */
-import raw from "../../middleware/route.async.wrapper.mjs";
+import raw from '../../middleware/route.async.wrapper.mjs';
 
 log.magenta(`user.router loaded...`);
 
 const router = express.Router();
 
 // parse json req.body on post routes
-router.use(express.json())
+router.use(express.json());
 
 // CREATES A NEW USER
 // router.post("/", async (req, res,next) => {
@@ -31,41 +31,50 @@ router.use(express.json())
 // });
 
 // CREATES A NEW USER
-router.post("/", raw( async (req, res) => {
+router.post(
+  '/',
+  raw(async (req, res) => {
     // TODO - validate req.body
     const user = await user_controller.create(req.body);
     res.status(200).json(user);
-}) );
+  }),
+);
 
 // GET ALL USERS
-router.get( "/",raw(async ( _ , res) => {
+router.get(
+  '/',
+  raw(async (_, res) => {
     const users = await user_controller.getAll();
     res.status(200).json(users);
-  })  
+  }),
 );
-
 
 // GETS A SINGLE USER
-router.get("/:id",raw(async (req, res) => {
-    const user = await user_controller.getOne(req.params.id)
-    if (!user) return res.status(404).json({ status: "No user found." });
+router.get(
+  '/:id',
+  raw(async (req, res) => {
+    const user = await user_controller.getOne(req.params.id);
+    if (!user) return res.status(404).json({ status: 'No user found.' });
     res.status(200).json(user);
-  })
+  }),
 );
 // UPDATES A SINGLE USER
-router.put("/:id",raw(async (req, res) => {
-    const user = await user_controller.updateOne(req.params.id,req.body);
+router.put(
+  '/:id',
+  raw(async (req, res) => {
+    const user = await user_controller.updateOne(req.params.id, req.body);
     res.status(200).json(user);
-  })
+  }),
 );
 
-
 // DELETES A USER
-router.delete("/:id",raw(async (req, res) => {
+router.delete(
+  '/:id',
+  raw(async (req, res) => {
     const user = await user_controller.deleteOne(req.params.id);
-    if (!user) return res.status(404).json({ status: "No user found." });
+    if (!user) return res.status(404).json({ status: 'No user found.' });
     res.status(200).json(user);
-  })
+  }),
 );
 
 export default router;
